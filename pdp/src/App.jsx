e6239f3b23636751ecf63744c517a8dddde907ed
fahad991 <fahad.amin@yellow.ai>
2022-02-26 16:34:15 +0600

    :sparkles: add route

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,18 +1,25 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './index.scss';
 import Header from 'home/Header';
 import Footer from 'home/Footer';
 import SafeComponent from './SafeComponent';
+import PDPContent from './PDPContent';
 
 const App = () => (
-  <div className='mt-10 text-3xl mx-auto max-w-6xl'>
-    <SafeComponent>
-      <Header />
-      Home page content
-      <Footer />
-    </SafeComponent>
-  </div>
+  <BrowserRouter>
+    <div className='mt-10 text-3xl mx-auto max-w-6xl'>
+      <SafeComponent>
+        <Header />
+
+        <Routes>
+          <Route path='/products/:id' element={<PDPContent />} />
+        </Routes>
+        <Footer />
+      </SafeComponent>
+    </div>
+  </BrowserRouter>
 );
 ReactDOM.render(<App />, document.getElementById('app'));
